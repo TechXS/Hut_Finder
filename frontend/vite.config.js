d643bc55dec9685f766995fcc5ceaca90cc5f77b
MesutEmpire <samwainaina01@gmail.com>
2024-02-23 13:33:38 +0300

    Modify configs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,21 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import {defineConfig, loadEnv} from "vite";
+import react from "@vitejs/plugin-react-swc";
+import * as path from "path";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({command, mode}) => {
+    // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+    const env = loadEnv(mode, process.cwd(), "");
+    return {
+        plugins: [react()],
+        resolve: {
+            alias: {
+                "@": path.resolve(__dirname, "./src"),
+            },
+        },
+        // vite config
+        define: {
+            __APP_ENV__: JSON.stringify(env.APP_ENV),
+        },
+    };
+});
